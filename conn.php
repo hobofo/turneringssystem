6f9c28f1731d566517914c223eb5383c998a15fb
Tomas Lieberkind <t.lieberkind@gmail.com>
2015-11-25 21:41:13 +0100

    Updated conn.php settings to read from environment variables instead of hardcoded strings

--- conn.php
+++ conn.php
@@ -1,6 +1,13 @@
 <?php
 
-$default_url = "http://localhost/";
-$link = mysql_connect('localhost', 'root', '')OR die(mysql_error());
-mysql_set_charset('utf8',$link);
-$db_selected = mysql_select_db('hobofo_dk', $link); 
+$dbHost = getenv('DB_HOST');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+$dbName = getenv('DB_NAME');
+
+// $default_url = "http://localhost/";
+$default_url = getenv('BASE_URL');
+
+$link = mysql_connect($dbHost, $dbUser, $dbPass) OR die(mysql_error());
+mysql_set_charset('utf8', $link);
+$db_selected = mysql_select_db($dbName, $link);
