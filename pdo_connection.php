9806b60e28e3277de62ed38a2d85910caaf092d9
Tomas Lieberkind <t.lieberkind@gmail.com>
2015-12-02 22:14:35 +0100

    Mulighed for at fjerne løs spiller

---
+++ pdo_connection.php
@@ -0,0 +1,9 @@
+<?php
+$dbHost = getenv('DB_HOST');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+$dbName = getenv('DB_NAME');
+
+$connectionString = "mysql:host=$dbHost;dbname=$dbName;charset=utf8";
+
+return new PDO($connectionString, $dbUser, $dbPass);
