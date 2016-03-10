5b807131ed4d41f1be18fef3f91b4b81cfddee71
Bjarke Hou Kammersgaard <bjarkehou@Bjarkes-MacBook-Pro.local>
2016-03-10 23:11:20 +0100

    fixed charset

--- rangliste/index.php
+++ rangliste/index.php
@@ -4,7 +4,7 @@
     <title>HBF Rangliste</title>
     <link rel="stylesheet" type="text/css" href="../css/reset.css">
     <link rel="stylesheet" type="text/css" href="rankings.css">
-
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
