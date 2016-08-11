345a1b4bb462bc22ad823dcfb5dac813b7669da4
Bjarke Hou Kammersgaard <bjarkehou@Bjarkes-MacBook-Pro.local>
2016-08-11 17:24:26 +0200

    Fixed a bug where deactivated users couldn't be reactivated, and also blocked for adding new user with same phone number.

--- ajax/bruger_ny.php
+++ ajax/bruger_ny.php
@@ -7,8 +7,12 @@ $telefon = $_POST["telefon"];
 $navn = $_POST["navn"];
 
 $query = mysql_query("SELECT * FROM hbf_brugere where telefon = '$telefon'") or die(mysql_error());
-if(mysql_num_rows($query) > 0){
-    echo "En bruger med dette telefonnummer findes allerede";
+if(mysql_num_rows($query) == 1){
+    $row = mysql_fetch_array($query);
+    if($row["deaktiv"] == 1) {
+    	mysql_query("UPDATE hbf_brugere SET deaktiv = 0 WHERE telefon = '$telefon'") or die(mysql_error());
+    	echo "$navn var deaktiveret, men er nu aktiv igen.";
+    }
 } else {
     $query = mysql_query("INSERT INTO hbf_brugere (telefon,navn) values ('$telefon','$navn')") or die(mysql_error());
     echo "$navn er oprettet som bruger. Velkommen!";
