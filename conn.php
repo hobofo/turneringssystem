d08c23031ce8bb8dd15ae295b6cd9ac800ffb17b
Kristian Schmidt <kst@siteimprove.com>
2018-12-12 14:00:01 +0100

    Fixed mysql_set_charset

--- conn.php
+++ conn.php
@@ -2,4 +2,4 @@
 
 $default_url = "http://localhost/";
 $link = mysqli_connect('localhost', 'root', '', 'hobofo_dk')OR die(mysqli_error($link));
-mysql_set_charset('utf8',$link);
+mysqli_set_charset($link, 'utf8');
